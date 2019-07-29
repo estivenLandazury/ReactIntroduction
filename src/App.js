8ea95858a24f4e2f0d65325398ad6aee1bc5d5e5
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-07-29 16:52:26 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+         <nav> 
+           
+
+
+         </nav>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+
+      <div className="container">
+
+        <h1> Hola panita</h1>
+
+      </div>
     </div>
   );
 }
